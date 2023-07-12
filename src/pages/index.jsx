61e68fca817b35479eda6404eaa22f2a6fa05318
Hüseyin Karataş <karatashuseyin2016@hotmail.com>
2023-07-13 01:58:01 +0300

    Fixes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,9 +14,9 @@ export default function Home() {
   const thirdSectionRef = useRef(null)
   const imgContainerRef = useRef(null)
   const firstContentRef = useRef(null)
-  const leftBigSpanRef = useRef(null)
+  const leftContainerRef = useRef(null)
   const leftSmallSpanRef = useRef(null)
-  const rightBigSpanRef = useRef(null)
+  const rightContainerRef = useRef(null)
   const rightSmallSpanRef = useRef(null)
   const { windowSize } = useWindow()
   const { firstRatio, secondRatio, thirdRatio } = useIntersectionRatio({
@@ -35,45 +35,60 @@ export default function Home() {
   //88-100 Opacity 0
 
   useEffect(() => {
-    if (secondRatio > 0 && secondRatio < 38) {
-      imgContainerRef.current.style = 'scale:' + (+secondRatio + 20) + '%;'
+    // console.log('third:' + thirdRatio)
+    if (secondRatio >= 0 && secondRatio < 38) {
+      imgContainerRef.current.style =
+        'scale:' + (+secondRatio * 1.2 + 20) + '%;'
     }
     if (secondRatio >= 52 && secondRatio < 64) {
       const eye = imgContainerRef.current
-      const leftBig = leftBigSpanRef.current
+      const leftBig = leftContainerRef.current
 
-      let eyeStyle = `scale: ${106 - +secondRatio}%;`
-      eyeStyle += ` transform:translateX(${52 - +secondRatio}px); `
+      let eyeStyle = `scale: ${117.6 - +secondRatio}%;`
+      eyeStyle += ` transform:translateX(${52 - +secondRatio * 1.2}px); `
       eye.style = eyeStyle
 
       let leftBigStyle =
-        'transform:translateY(-' + ((+secondRatio - 52) / 12) * 50 + 'vh);'
+        'transform:translateY(' +
+        (70 - ((+secondRatio - 52) / 12) * 70).toFixed(2) +
+        'vh);'
+      // console.log(leftBigStyle)
       leftBig.style = leftBigStyle
     }
     if (secondRatio >= 64 && secondRatio < 70) {
       const leftSmall = leftSmallSpanRef.current
-      let leftSmallStyle =
-        'transform:translateY(-' + ((+secondRatio - 64) / 6) * 50 + 'vh);'
+
+      let leftSmallStyle = 'opacity:' + ((+secondRatio - 64) / 6) * 100 + '%;'
       leftSmall.style = leftSmallStyle
     }
     if (secondRatio >= 70 && secondRatio < 82) {
-      const rightBig = rightBigSpanRef.current
+      const rightBig = rightContainerRef.current
       let rightBigStyle =
-        'transform:translateY(-' + ((+secondRatio - 70) / 12) * 50 + 'vh);'
+        'transform:translateY(' +
+        (70 - ((+secondRatio - 70) / 12) * 70).toFixed(2) +
+        'vh);'
       rightBig.style = rightBigStyle
     }
     if (secondRatio >= 82 && secondRatio < 88) {
       const rightSmall = rightSmallSpanRef.current
-      let rightSmallStyle =
-        'transform:translateY(-' + ((+secondRatio - 82) / 6) * 50 + 'vh);'
+      let rightSmallStyle = 'opacity:' + ((+secondRatio - 82) / 6) * 100 + '%;'
       rightSmall.style = rightSmallStyle
+    }
+    if (secondRatio >= 88 && secondRatio < 100) {
+      secondSectionRef.current.style =
+        'opacity:' + (100 - ((+secondRatio - 88) / 12) * 100) + '%'
+      thirdSectionRef.current.style = `transform:translateY(-${
+        ((+secondRatio - 88) / 12) * 100
+      }vh)`
+    }
+    if (secondRatio >= 100) {
     }
   }, [secondRatio])
 
   return (
     <RootLayout>
       <section
-        className={`h-[100vh] first pt-48 min-h-max`}
+        className={`h-[100vh] first pt-48 min-h-max fade-in`}
         ref={firstSectionRef}
       >
         <CustomContainer className="maincontainer">
@@ -99,17 +114,20 @@ export default function Home() {
           </div>
         </CustomContainer>
       </section>
-      <section ref={secondSectionRef} className="second h-[200vh] bg-red-500 ">
+      <section ref={secondSectionRef} className="second h-[200vh]">
         <CustomContainer>
           <div className="w-full h-[100vh] sticky top-0 ">
-            <div className="w-full relative h-full">
-              <div className="absolute top-[50%] translate-y-[50vh]">
-                <h3 className="text-[90px] leading-none" ref={leftBigSpanRef}>
+            <div className="w-full relative h-full -translate-y-12">
+              <div
+                className="absolute top-[50%] translate-y-[70vh]"
+                ref={leftContainerRef}
+              >
+                <h3 className="text-[90px] leading-none">
                   Başarının <br />
                   anahtarı
                 </h3>
                 <div
-                  className="w-full text-center mt-2 "
+                  className="w-full text-center mt-2 opacity-0 "
                   ref={leftSmallSpanRef}
                 >
                   <span className={`${gravitasOne.className}`}>“</span>
@@ -119,7 +137,7 @@ export default function Home() {
               </div>
 
               <div
-                className=" flex w-[100%] justify-center transition-transform duration-[1s] absolute left-0"
+                className=" flex w-[100%] justify-center transition-transform duration-[1s] fade-in "
                 ref={imgContainerRef}
               >
                 <Image
@@ -131,13 +149,16 @@ export default function Home() {
                 />
               </div>
 
-              <div className="absolute top-[50%] right-0 translate-y-[50vh]">
-                <h3 className=" text-[90px] leading-none" ref={rightBigSpanRef}>
+              <div
+                className="absolute top-[50%] right-0 translate-y-[70vh]"
+                ref={rightContainerRef}
+              >
+                <h3 className=" text-[90px] leading-none">
                   İnsanların <br />
                   Bakışları
                 </h3>
                 <div
-                  className="w-full text-center  mt-2 "
+                  className="w-full text-center mt-2 opacity-0"
                   ref={rightSmallSpanRef}
                 >
                   <span className={`${gravitasOne.className} `}>“</span>
@@ -149,10 +170,9 @@ export default function Home() {
           </div>
         </CustomContainer>
       </section>
-      <section
-        ref={thirdSectionRef}
-        className="h-[100vh] bg-blue-500 third"
-      ></section>
+      <section ref={thirdSectionRef} className="h-[100vh] bg-blue-500 third">
+        hey
+      </section>
     </RootLayout>
   )
 }
