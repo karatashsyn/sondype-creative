ce998f433587b961c28abf4dfd77b0ae5fff4b85
Hüseyin Karataş <karatashuseyin2016@hotmail.com>
2023-07-23 00:18:36 +0300

    Updates

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/sc.png" />
+          <link rel="icon" href="./../../public/sc.png" />
         </Head>
         <body>
           <Main />
