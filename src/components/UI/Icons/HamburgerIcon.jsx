ec8a6b95709adf29f5d4e3ad708f077c5952cb70
Hüseyin Karataş <karatashuseyin2016@hotmail.com>
2023-07-22 23:52:46 +0300

    Updates

---
+++ src/components/UI/Icons/HamburgerIcon.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Hamburger() {
+  return (
+    <svg
+      width="34"
+      height="22"
+      viewBox="0 0 34 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="34" height="4" rx="1" fill="white" />
+      <rect width="34" height="4" rx="1" fill="white" />
+      <rect y="9" width="34" height="4" rx="1" fill="white" />
+      <rect y="9" width="34" height="4" rx="1" fill="white" />
+      <rect y="18" width="34" height="4" rx="1" fill="white" />
+      <rect y="18" width="34" height="4" rx="1" fill="white" />
+    </svg>
+  )
+}
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,29 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SondypeIcon from '../UI/Icons/SondypeIcon'
 import Link from 'next/link'
 import MainButton from '../UI/Buttons/MainButton'
+import HamburgerMenu from '../UI/Icons/HamburgerIcon'
+import useWindow from '@/hooks/useWindow'
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+  const { windowSize } = useWindow()
+  useEffect(() => {
+    console.log(windowSize)
+    if (windowSize > 768) {
+      setIsOpen(false)
+    }
+  }, [windowSize])
   return (
     <nav
-      className="navbar fixed left-0 right-0 w-[100vw] lg:px-[80px] px-[24px] flex justify-between md:pt-7 pt-3 pb-2 gap-8 bg-black z-10 "
+      className={`navbar fixed left-0 right-0 w-[100vw]lg:px-[80px] px-[24px] flex justify-between md:pt-7 pt-3 pb-2 gap-8 bg-black z-50`}
       style={{ boxShadow: '2px 8px 16px 8px black' }}
     >
+      {/* backdrop div */}
+      <div
+        onClick={() => {
+          setIsOpen(false)
+        }}
+        className={`fixed inset-0 ${
+          isOpen ? 'bg-black opacity-75' : ''
+        } sm:hidden sm:pointer-events-none ${isOpen ? '' : 'hidden'}`}
+      ></div>
       <Link href={'/'} className="cursor-pointer">
-        <SondypeIcon />
+        <div className="w-[200px] md:w-[240px]">
+          <SondypeIcon />
+        </div>
       </Link>
-      <ul className="flex gap-12 items-center">
+      <div
+        className="sm:hidden mr-4 z-50 cursor-pointer"
+        onClick={() => {
+          setIsOpen((val) => !val)
+        }}
+      >
+        <HamburgerMenu />
+      </div>
+      <ul
+        className={`flex gap-12 items-center max-sm:fixed max-sm:left-0 max-sm:right-0 max-sm:top-0 max-sm:flex-col  duration-200 max-sm:overflow-hidden max-sm:bg-gray-900 z-40 ${
+          isOpen ? 'max-sm:h-[70vh] pt-[80px]' : 'max-sm:h-0'
+        }`}
+      >
         <Link href={'/'}>
-          <li className="text-[15px]">Anasayfa</li>
+          <li className="text-[15px] max-sm:font-bold">Anasayfa</li>
         </Link>
         <Link href={'/contact'}>
-          <li className="text-[15px]">İletişim</li>
+          <li className="text-[15px] max-sm:font-bold">İletişim</li>
         </Link>
 
         <Link href={'/aboutUs'}>
-          <li className="text-[15px]">Hakkımızda</li>
+          <li className="text-[15px] max-sm:font-bold">Hakkımızda</li>
         </Link>
         <Link href={'/blog'}>
-          <li className="text-[15px]">Blog</li>
+          <li className="text-[15px] max-sm:font-bold">Blog</li>
         </Link>
       </ul>
     </nav>
