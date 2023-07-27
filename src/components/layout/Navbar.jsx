e201b52f93c430554bcc7990905329487e4d7ce5
Hüseyin Karataş <karatashuseyin2016@hotmail.com>
2023-07-27 19:41:04 +0300

    Updates

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useMemo, useState } from 'react'
 import SondypeIcon from '../UI/Icons/SondypeIcon'
 import Link from 'next/link'
 import MainButton from '../UI/Buttons/MainButton'
 import HamburgerMenu from '../UI/Icons/HamburgerIcon'
 import useWindow from '@/hooks/useWindow'
+import { useRouter } from 'next/router'
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
   const { windowSize } = useWindow()
+  const { pathname } = useRouter()
+  const currentPath = useMemo(() => pathname.split('/')[1], [pathname])
+
   useEffect(() => {
     if (windowSize > 768) {
       setIsOpen(false)
@@ -17,7 +21,6 @@ export default function Navbar() {
       className={`navbar fixed left-0 right-0 w-[100vw]lg:px-[80px] px-[24px] flex justify-between md:pt-7 pt-3 pb-2 gap-8 bg-black z-50`}
       style={{ boxShadow: '2px 8px 16px 8px black' }}
     >
-      {/* backdrop div */}
       <div
         onClick={() => {
           setIsOpen(false)
@@ -45,17 +48,41 @@ export default function Navbar() {
         }`}
       >
         <Link href={'/'}>
-          <li className="text-[15px] max-sm:font-bold">Anasayfa</li>
+          <li
+            className={`text-[15px] max-sm:font-bold  ${
+              !currentPath.length ? 'activeLink' : ''
+            }`}
+          >
+            Anasayfa
+          </li>
         </Link>
         <Link href={'/contact'}>
-          <li className="text-[15px] max-sm:font-bold">İletişim</li>
+          <li
+            className={`text-[15px] max-sm:font-bold  ${
+              currentPath === 'contact' ? 'activeLink' : ''
+            }`}
+          >
+            İletişim
+          </li>
         </Link>
 
         <Link href={'/aboutUs'}>
-          <li className="text-[15px] max-sm:font-bold">Hakkımızda</li>
+          <li
+            className={`text-[15px] max-sm:font-bold  ${
+              currentPath === 'aboutUs' ? 'activeLink' : ''
+            }`}
+          >
+            Hakkımızda
+          </li>
         </Link>
         <Link href={'/blog'}>
-          <li className="text-[15px] max-sm:font-bold">Blog</li>
+          <li
+            className={`text-[15px] max-sm:font-bold  ${
+              currentPath === 'blog' ? 'activeLink' : ''
+            }`}
+          >
+            Blog
+          </li>
         </Link>
       </ul>
     </nav>
